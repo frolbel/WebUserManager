64be9125c399e35e40b783e8d87f4fcf768efea8
Alexander Belkin <AlexBelkin@list.ru>
2020-02-26 21:16:55 +0400

    feature(model): добавление модели User

---
+++ WebUserManager/Models/User.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUserManager.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    /// <summary>
+    /// Описание модели данных пользователя
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Получает или задает идентификатор пользователя
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Получает или задает имя пользователя
+        /// </summary>
+        [StringLength(50), Required]
+        [Display(Name = "Имя")]
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Получает или задает фамилию пользователя
+        /// </summary>
+        [StringLength(50), Required]
+        [Display(Name = "Фамилия")]
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Получает или задает адрес пользователя
+        /// </summary>
+        [StringLength(255)]
+        [Display(Name = "Адрес")]
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Получает или задает телефон пользователя
+        /// </summary>
+        [StringLength(50)]
+        [Display(Name = "Тел.")]
+        public string Phone { get; set; }
+
+        /// <summary>
+        /// Получает или задает возраст пользователя
+        /// </summary>
+        [Display(Name = "Возраст")]
+        public int Age { get; set; }
+
+    }
+}
